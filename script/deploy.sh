857a1ea888316d2aec8b8cd543ab77fba839550f
Kris Longmore <kris@robotwealth.com>
2020-07-17 13:03:19 +1000

    update yml

--- script/deploy.sh
+++ script/deploy.sh
@@ -2,6 +2,6 @@
 set -e
 
 echo "Starting deploy"
-echo "${DOCKER_PWD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
+echo "${DOCKER_PASSWORD}" | docker login --username "${DOCKER_LOGIN}" --password-stdin
 docker tag zutherb/monolithic-shop:latest $DOCKER_LOGIN/monolithic-shop:latest
 docker push $DOCKER_LOGIN/monolithic-shop:latest
